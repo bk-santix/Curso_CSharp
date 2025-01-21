15983ba9f74630f8903de41840f9d377c3c32b8c
Bruno <bruno.lfsantos1@senacsp.edu.br>
2025-01-21 19:30:30 -0300

    EX009

--- Ex006/Program.cs
+++ Ex006/Program.cs
@@ -1,46 +1,50 @@
-﻿using System;
-using System.ComponentModel.Design;
-
-
-namespace Ex006
+﻿namespace Ex006
 {
-    class Program
+    internal class Program
     {
         static void Main(string[] args)
         {
-            double Nota_final = 70;
+            double nota_final, n1, n2, n3, n4;
             string resultado;
 
-            //Condicões 
+            Console.WriteLine("Digite a primeira nota: ");
+            n1 = Convert.ToDouble(Console.ReadLine());
 
-            if (Nota_final >= 60)
-            {
-                resultado = "Aprovado";
+            Console.WriteLine("Digite a segunda nota: ");
+            n2 = Convert.ToDouble(Console.ReadLine());
 
-            }
-            else if (Nota_final >= 40)
+            Console.WriteLine("Digite a terceira nota: ");
+            n3 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite a quarta nota: ");
+            n4 = Convert.ToDouble(Console.ReadLine());
+
+            nota_final = (n1 + n2 + n3 + n4) / 4;
+
+            if (nota_final >= 60)
             {
-                resultado = "Recuperação";
+                if (nota_final >= 90)
+                {
+                    resultado = "Aprovado com louvor";
+                }
+                else
+                {
+                    resultado = "Aprovado";
+                }
             }
             else
             {
-                resultado = "Reprovado";
+                if(nota_final >= 40)
+                {
+                    resultado = "Recuperação";
+                }
+                else
+                {
+                    resultado = "Reprovado";
+                }
             }
-            
-          
 
-            Console.WriteLine("O aluno foi {0}", resultado);
-
-            
-            
-            
-
-
-            
-            
-
-
-            
+            Console.WriteLine("Nota do aluno: {0} - Resultado: {1}", nota_final, resultado);
         }
     }
 }
---
+++ Ex009/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Ex009
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+           
+            Console.WriteLine("Progama para testar triagulos");
+            
+
+
+
+            int ladoA, ladoB, ladoC;
+
+            Console.WriteLine("Digite lado A: ");
+            ladoA = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine("Digite lado B: ");
+            ladoB = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine("Digite lado C: ");
+            ladoC = Convert.ToInt32(Console.ReadLine());
+
+            //Codicões
+            
+            
+            if ((ladoA < ladoB + ladoC) && (ladoB < ladoA + ladoC) && (ladoC < ladoA + ladoC))
+            {
+                Console.WriteLine("Os valores informados formam um triagulo");
+
+            }
+            else
+            {
+                Console.WriteLine("Os valores não formam um triagulo");
+            }
+             
+
+
+
+
+
+        }
+    }
+}
