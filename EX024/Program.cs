8cc4dce9244b708f2135523459401ad75122193d
Bruno <bruno.lfsantos1@senacsp.edu.br>
2025-01-23 21:35:52 -0300

    EX024

---
+++ EX024/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace EX024
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string Gênero;
+
+            Console.WriteLine("Digite o gênero: ");
+            Gênero = Console.ReadLine().ToUpper();
+
+            while(Gênero != "F" && Gênero != "M")
+            {
+                Console.Write("Digite o Gênero: ");
+                Gênero = Console.ReadLine();
+            }
+
+            Console.WriteLine("Bem vindos ao curso C#");
+
+
+
+
+
+
+
+        }
+    }
+}
