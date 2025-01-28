6091cf68fa2035e982eebf13e3398567ecf12106
Bruno <bruno.lfsantos1@senacsp.edu.br>
2025-01-28 19:46:09 -0300

    Senha

---
+++ EX026/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace EX026
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int num = 3;
+
+            while (num > 5)
+            {
+                num++;
+                Console.WriteLine("Curso C#");
+            } 
+
+            do
+            {
+                num++;
+                Console.WriteLine("Curso C#");
+
+
+
+            }while (num < 5);
+
+
+
+
+        }
+    }
+}
---
+++ Senha/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace Senha
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string Senha = "123"; // Senha correta 
+            string SenhaDigitada; // Senha Digitada pelo usuario 
+            int Tentativas = 0; // Contador de tentativas 
+
+
+
+            // Loop Para solicitar a senha do usuario !!
+            do
+            {
+                Console.Clear();
+                Console.Write("Digite a Sua senha:");
+                SenhaDigitada = Console.ReadLine();
+                Tentativas++;
+
+                if (SenhaDigitada != Senha && Tentativas < 3)
+                {
+                    Console.WriteLine("Senha incorreta!");
+                    Console.WriteLine("Você ainda tem " + (3 - Tentativas) + " Tentativa(s) ");
+                    Console.ReadKey();
+                }
+                else if (SenhaDigitada != Senha && Tentativas == 3)
+                {
+                    Console.WriteLine("Senha incorreta - Seu acesso foi bloaqueado");
+                    return;
+                }
+                
+                   
+                
+
+
+
+            } while (SenhaDigitada != Senha);
+           
+            
+            Console.Clear() ;
+            Console.WriteLine("Senha Correta");
+            Console.WriteLine("Tentativas:" + Tentativas);
+
+
+
+        }
+    }
+}
