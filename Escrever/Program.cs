29369b4c4faf203a135b9e528abe35d48cbc4c28
Bruno <bruno.lfsantos1@senacsp.edu.br>
2025-01-30 21:50:16 -0300

    Escrever

---
+++ Escrever/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Escrever
+{
+    internal class Program
+    {
+       
+        static void Escrever(string Texto)
+
+        {
+            // Padleft pode centralizar o texto subtraindo o numero digitado no metodo
+            // * Mutiplica o numero + depois do lenghth
+            int tam = Texto.Length + 4;
+            string linha = new('*', tam);
+            string TextoCentralizado = Texto.PadLeft(tam - 2).PadRight(tam - 2);
+            Console.WriteLine(linha);
+            Console.WriteLine(TextoCentralizado);
+            Console.WriteLine(linha);
+
+        }
+        
+        
+        static void Main(string[] args)
+        {
+            Escrever("Bruno luis Ferreira Dos Santos");
+            Console.WriteLine();
+            Escrever("Curso C#");
+            Console.WriteLine();
+
+
+        }
+    }
+}
