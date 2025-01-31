4fed91a9d55ef985dab476c95af8079d9ef52496
Bruno <bruno.lfsantos1@senacsp.edu.br>
2025-01-30 21:31:57 -0300

    Area do Triangulo

---
+++ Area do Triangulo/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Area_do_Triangulo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Área do Triângulo");
+
+
+
+            Console.WriteLine("Digite o lado A");
+            double ladoA = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite o lado B");
+            double ladoB = Convert.ToDouble(Console.ReadLine());
+            AreadoTriagulo(ladoA, ladoB);
+
+
+
+
+        }
+
+        static void AreadoTriagulo(double a, double b)
+        {
+            double areadoTriagulo = a * b / 2 ;
+
+            Console.WriteLine($"O resultado é: {a} x {b} = {areadoTriagulo}");
+        }
+    }
+}
